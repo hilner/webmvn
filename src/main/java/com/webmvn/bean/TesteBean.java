fd7380dcd0ee44887f372ca5ba494996d04a582c
Levy Moreira <levymoreira.ce@gmail.com>
2013-05-09 10:31:42 -0300

    Teste navegação com bean

--- src/main/java/com/webmvn/bean/TesteBean.java
+++ src/main/java/com/webmvn/bean/TesteBean.java
@@ -1,12 +1,46 @@
 package com.webmvn.bean;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean
-@RequestScoped
-public class TesteBean {
+@SessionScoped
+public class TesteBean implements Serializable{
+
+	//Constants
+	private static final long serialVersionUID = 1L;
 	
-	public TesteBean(){}	
+	//Properties
+	private String umaString = "";
+	
+	//Constructors
+	public TesteBean(){}
+	
+	//Methods
+	public String goToPage1(){
+		setUmaString("chegando na pg1");
+		return "page1";
+	}
+	
+	public String goToPage2(){
+		setUmaString("chegando na pg2");
+		return "page2";
+	}
+	
+	public String goToPage3(){
+		setUmaString("chegando na pg3");
+		return "page3";
+	}
+
+	//Getters and Setters
+	public String getUmaString() {
+		return umaString;
+	}
+
+	public void setUmaString(String umaString) {
+		this.umaString = umaString;
+	}
 	
 }
